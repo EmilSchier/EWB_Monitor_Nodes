ffd62f1d341258ada69201e2860fcf581d005088
Jes <kontaktjes@gmail.com>
2019-12-04 13:41:11 +0100

    Tested version. Long range conf unstable.

--- test/Range_tests/Range_tests.ino
+++ test/Range_tests/Range_tests.ino
@@ -4,16 +4,22 @@
 #include "RV-3028-C7.h"
 
 //RH_RF95/RFM96 driver;
-RH_RF95 driver(4,2);   // pins for 1284P
+RH_RF95 driver(4,2);   // pins for Mega
 //RH_RF95 driver(53,2);   // pins for Mega
 
 // Class to manage message delivery and receipt, using the driver declared above
-RHMesh manager(driver, NODE1_ADDRESS);
+RHMesh manager(driver, NODE2_ADDRESS);
 
 RV3028 rtc;
 
 // Defines
 #define RTC_INTERRUPT_PIN 10
+
+// Pinheader P5
+#define _PC4 20
+#define _PC5 21
+#define _PC6 22
+#define _PC7 23
 
 // defines the time and date to be set
 #define SECONDS     0
@@ -24,7 +30,7 @@ RV3028 rtc;
 #define MONTH       1
 #define YEAR        2019
 
-#define TIMER_TIME        10 // the time, 0 = dissabled
+#define TIMER_TIME        20 // the time, 0 = dissabled
 #define TIMER_UNIT        UNIT_SECOND
 /*****************
  Determines the unit used for the countdown time
@@ -43,7 +49,6 @@ const bool sender = true;
 const bool listener = true;
 const float radioFrequency = 868.0;
 const uint8_t txPower = 23;  // Comment for default
-const uint16_t waitTime = 10000;
 
 //RTC interrupt service routine
 void rtcISR() {
@@ -52,10 +57,10 @@ void rtcISR() {
 
 void setup() {
   pinMode(RTC_INTERRUPT_PIN, INPUT);
-  pinMode(PC4,INPUT);
-  pinMode(PC5,INPUT);
-  pinMode(PC6,INPUT);  
-  pinMode(PC7,INPUT);
+  pinMode(_PC4,INPUT);
+  pinMode(_PC5,INPUT);
+  pinMode(_PC6,INPUT);  
+  pinMode(_PC7,INPUT);
   
   Serial.begin(9600);
   Wire.begin();
@@ -67,41 +72,44 @@ void setup() {
   driver.setFrequency(radioFrequency);
   driver.setTxPower(txPower,false);
 
-  if (PC4) {
+  manager.clearRoutingTable();          // clear routing table
+
+  if (digitalRead(_PC4)) {
     driver.setModemConfig(RH_RF95::Bw500Cr45Sf128);  // Fast and short range
     Serial.println("Config set: Bw500Cr45Sf128");
   }
-  else if (PC5) {
+  else if (digitalRead(_PC5)) {
     driver.setModemConfig(RH_RF95::Bw125Cr45Sf128);  // Medium range
-    Serial.println("Config set: Bw500Cr45Sf128");
+    Serial.println("Config set: Bw125Cr45Sf128");
   }
-  else if (PC6) {
+  else if (digitalRead(_PC6)) {
     driver.setModemConfig(RH_RF95::Bw31_25Cr48Sf512);  // Slow and long range
     Serial.println("Config set: Bw31_25Cr48Sf512");
   }
-  else if (PC7) {
+  else if (digitalRead(_PC7)) {
     driver.setModemConfig(RH_RF95::Bw125Cr48Sf4096);  // Slow and long range
     Serial.println("Config set: Bw125Cr48Sf4096");
   }
-  else {
-    Serial.println("Error - no input on PC4 to PC7.");
-    while(true);
-  }
-
-  manager.clearRoutingTable();          // clear routing table
+  else
+    Serial.println("Error - no input on PC4 to PC7. Default conf used.");
 
   // setup RTC 
-  rtc.begin();
+  if (!rtc.begin())
+    Serial.println("RTC setup failed");
   if (rtc.setTime(SECONDS, MINUTES, HOURS, WEEKDAY, DATE, MONTH, YEAR)) {
-    Serial.println("time set");
+    Serial.println("RTC initialised. Time set");
   }
   rtc.setCountdownTimer(TIMER_TIME, TIMER_UNIT, TIMER_REPEAT);
   rtc.enableCountdownTimer(); // uncomment to enable the countdown timer
   //rtc.disableCountdownTimer(); // Uncomment to disable the countdown timer
+
+  //rtc.enableTimerInterrupt;
   
   rtc.clearInterrupts();
   attachInterrupt(digitalPinToInterrupt(RTC_INTERRUPT_PIN), rtcISR, FALLING);  
 }
+
+uint8_t data[] = "Test";
 
 void loop() {
 
@@ -112,7 +120,7 @@ void loop() {
 
   listenForMessages(listener);
   rtcIntHandler();
-
+  
 }
 
 // Listen for new messages
@@ -125,11 +133,15 @@ void listenForMessages(bool run){
   uint8_t from;
   if (manager.recvfromAck(buf, &len, &from))
   {
+    // Send a reply back to the originator client
+    //if (manager.sendtoWait(data, sizeof(data), from) != RH_ROUTER_ERROR_NONE)
+      //Serial.println("Reply failed");
+      
     Serial.print("got request from : 0x");
     Serial.println(from, HEX);
     Serial.print("Received data: ");
     Serial.println((char*)buf);
-    uint16_t Rssi = driver.lastRssi();
+    int16_t Rssi = driver.lastRssi();
     Serial.print("Rssi: ");
     Serial.println(Rssi);
   }
@@ -141,25 +153,29 @@ void sendMessage(bool run){
     return;
   }
   
-  Serial.println("Sending to NODE2_ADDRESS");
-  uint8_t payload[] = "8 bytes!";  // messageID i stedet? 
-  long int recTime = 0;
-  long int sendTime = millis();
+  Serial.println("Sending to NODE1_ADDRESS");
+  uint8_t payload[] = "8 bytes";  // messageID i stedet? 
+  uint32_t recTime = 0;
+  uint32_t sendTime = millis();
+  int error;
   // Send start timestamp, which gives an eight byte payload. 
-  if (manager.sendtoWait(payload, sizeof(payload), NODE2_ADDRESS) == RH_ROUTER_ERROR_NONE)
+  if ( (error = manager.sendtoWait(payload, sizeof(payload), NODE1e_ADDRESS) ) == RH_ROUTER_ERROR_NONE)
   {
-    int recTime = millis();  // is run as soon as the data has been send and acknowledged by the next node. 
+    recTime = millis();  // is run as soon as the data has been send and acknowledged by the next node. 
     
     // It has been reliably delivered to the next node.
-    long int resTime = recTime - sendTime;
+    uint16_t resTime = recTime - sendTime;
     Serial.print("Message send and ACK received. Message ID: ");
     Serial.println(messageID);
     Serial.print("resTime = ");
     Serial.println(resTime);
+    messageID++;
   }
   else
   {
     Serial.println("No ack, are other nodes running or too far away?");
+    Serial.print("Error code: ");
+    Serial.println(error);
   }   
 }
 
@@ -192,7 +208,7 @@ void rtcIntHandler() {
     //Serial.print("Timer at: ");
     //Serial.println(rtc.stringTime());
 
-    sendMessage(sender);
+    sendMessage(sender);  // Kun ved RTC interrupt
     
   }
   /*    if ((flags & _BV(STATUS_UF)))
